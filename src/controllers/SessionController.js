4418cb58cc135d9151765cfff0e04bcc5230c0f1
Aécio Cleber <ramsolucoes@gmail.com>
2019-10-06 09:38:17 -0300

    email required

--- src/controllers/SessionController.js
+++ src/controllers/SessionController.js
@@ -4,6 +4,9 @@ const User = require("../models/User");
 module.exports = {
   async store(req, res) {
     const { email } = req.body;
+
+    if(!email) return res.status(400).json({error: 'Email not send'});
+
     let user = await User.findOne({ email });
 
     if (!user) user = await User.create({ email });
--- src/models/User.js
+++ src/models/User.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-    email:String,
+    email: {
+        type : String,
+        required : true
+    },
 });
 
 module.exports = mongoose.model('User', UserSchema);
